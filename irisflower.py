c5ac58758e9b435bf4e717779b778591cfb0013a
vyshnaviK18 <113926615+vyshnaviK18@users.noreply.github.com>
2023-08-13 14:21:53 +0530

    Add files via upload

---
+++ irisflower.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+import plotly.io as io
+import plotly.express as px
+iris = pd.read_csv("C:\\Users\\vyshn\\OneDrive\\Desktop\\fom\\iris.csv")
+print(iris.head()) 
+print()
+print(iris.describe())
+print("Target Labels", iris["species"].unique())
+
+
+fig = px.scatter(iris, x="sepal_width", y="sepal_length", color="species")
+fig.show()
+
+x = iris.drop("species", axis=1)
+y = iris["species"]
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2,random_state=0)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(x_train, y_train)
+
+x_new = np.array([[6, 2.9, 1, 0.2]])
+prediction = knn.predict(x_new)
+print("Prediction: {}".format(prediction))
